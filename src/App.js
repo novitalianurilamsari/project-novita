a7c17e076dea55e4cac75011480b1feb7f5f6649
novitalianurilamsari <novitalianurilamsari@gmail.com>
2022-04-21 17:56:33 +0700

    add page songPlaylist

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import logo from './logo.svg';
+import logo from './Spotify.svg';
 import './App.css';
+import data from './data/data.js';
+import React, { useEffect } from 'react';
 
+const URL =
+  'https://gist.githubusercontent.com/aryapradipta9/e6492383477803b233916e01f36d5465/raw/66942c739d66d3774303f84071696aa865a07077/single-sample.json';
+
+//mendapatkan data
+const getSongs = async () => {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    // menampilkan data
+    console.log(data);
+    return data;
+  } catch (error) {
+    // akan muncul alert error
+    alert(error);
+    return error;
+  }
+};
+
+const SPOTIFY_CLIENT_ID = process.env.REACT_APP_YOUR_SPOTIFY_APP;
+  
 function App() {
-  console.log(process.env.REACT_APP_YOUR_GIGIH_APP_API_KEY);
+  useEffect(() => {
+    console.log(SPOTIFY_CLIENT_ID);
+    getSongs();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://hainovitalia.blogspot.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ini blog novi
-        </a>
+        <h1>Create Playlist<span>...</span></h1>
+        <h2>now you can create your own playlist for free</h2>
       </header>
-    </div>
-  );
-}
+      <body>
+          <div class='songPlaylist'>
+              <img className='album-image' src={data.album.images[0].url} alt="album-cover"/>
+              <p className='text-title' id="trackTitle">{data.name}</p>
+              <p className='text-artist'>{data.album.artists[0].name}</p>
+              <p className='text-album'>{data.album.name}</p>
+              <button type='button' className='button-action' value='Select'> Select</button>
+          </div>
+      </body>
+      </div>
+    );
+  }
 
 export default App;
